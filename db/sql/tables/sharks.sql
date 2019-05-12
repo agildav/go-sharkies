5be16a7b4de4b2b54263c48a9e75656fe235ba87
Alberto A. Gil D <agildav@gmail.com>
2019-05-12 10:38:56 -0400

    Modified table struct

--- db/sql/tables/sharks.sql
+++ db/sql/tables/sharks.sql
@@ -3,12 +3,12 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS public.sharks CASCADE;
 
 CREATE TABLE public.sharks (
-	id serial NOT NULL,
-	name varchar(160) NOT NULL,
-	bname varchar(380) NOT NULL,
+  id serial NOT NULL,
+  name varchar(160) NOT NULL,
+  bname varchar(380) NOT NULL,
   description varchar(2048) NULL,
   image varchar(2048) NOT NULL,
-	CONSTRAINT sharks_pk PRIMARY KEY (id)
+  CONSTRAINT sharks_pk PRIMARY KEY (id)
 );
 
 COMMIT;
